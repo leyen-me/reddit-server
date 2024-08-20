bcf3d8d92cb5fb64ff2570a916a77332d20a2680
DIFFFFFFT <672228275@qq.com>
2024-08-20 18:48:31 +0800

    fix trans

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -4,42 +4,77 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const { sentence } = await request.json();
+  const sentences: string[] = await request.json();
 
-  if (!sentence) {
+  if (!sentences) {
     return NextResponse.json(
       { error: "Missing sentence parameter" },
       { status: 400 }
     );
   }
 
-  try {
-    const translationResult = await deepLTranslator(sentence);
-
-    // 存储到数据库
-    const translation = await prisma.translation.create({
-      data: {
-        sentence: sentence,
-        translation: translationResult.translations[0].text,
+  // 查询数据库已存在的数据
+  const existingTranslations = await prisma.translation.findMany({
+    where: {
+      sentence: {
+        in: sentences,
       },
-    });
-    // await prisma.translation.create({
-    return NextResponse.json({ translation: translationResult });
-  } catch (error) {
-    return NextResponse.json(
-      { error: (error as Error).message },
-      { status: 500 }
+    },
+  });
+
+  // 计算要翻译的句子
+  const sentencesToTranslate = sentences.filter(
+    (s: string) => !existingTranslations.map((et) => et.sentence).includes(s)
+  );
+
+  let buildTranslationResult: any[] = [];
+
+  // 将要翻译的数据进行翻译
+  if (sentencesToTranslate.length > 0) {
+    const translationResult = await deepLTranslator(sentencesToTranslate);
+    buildTranslationResult = sentencesToTranslate.map(
+      (s: string, i: number) => {
+        return {
+          sentence: s,
+          translation: translationResult.translations[i].text,
+        };
+      }
     );
+    // 将翻译结果批量存储到数据库
+    await prisma.translation.createMany({ data: buildTranslationResult });
   }
+
+  // 将翻译的最终结果，按请求传入的sentences顺序进行组合
+  const finalTranslation = sentences.map((s: string) => {
+    const translation = existingTranslations.find((et) => et.sentence === s);
+    const translationResult = buildTranslationResult.find(
+      (t: any) => t.sentence === s
+    );
+    return translation
+      ? translation.translation
+      : translationResult?.translation;
+  });
+
+  return NextResponse.json(finalTranslation);
+}
+
+interface TranslationResultItem {
+  detected_source_language: string;
+  text: string;
+}
+
+interface TranslationResult {
+  translations: TranslationResultItem[];
 }
 
 // DeepL 翻译函数
-async function deepLTranslator(sentence: string) {
+async function deepLTranslator(
+  sentences: string[]
+): Promise<TranslationResult> {
   const data = {
-    text: [sentence],
+    text: sentences,
     target_lang: "ZH",
   };
-
   const response = await fetch("https://api-free.deepl.com/v2/translate", {
     method: "POST",
     headers: {
@@ -49,5 +84,5 @@ async function deepLTranslator(sentence: string) {
     body: JSON.stringify(data),
   });
 
-  return await response.json();
+  return (await response.json()) as TranslationResult;
 }
